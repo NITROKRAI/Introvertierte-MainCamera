6b8077fe6aec15a78871f2c1f6dffa5b522927fd
Diet3r <86099270+Diet3r@users.noreply.github.com>
2021-07-15 19:03:03 +0200

    prototype
    start

--- Assets/Scripts/UI/IngameUI.cs
+++ Assets/Scripts/UI/IngameUI.cs
@@ -7,18 +7,18 @@ public class IngameUI : MonoBehaviour
     public TMPro.TextMeshProUGUI playerLives;
     public TMPro.TextMeshProUGUI playerLives2;
 
-    private PlayerMovement player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        player = FindObjectOfType<PlayerMovement>();
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerLives.text = player.lives.ToString();
-        playerLives2.text = player.lives.ToString();
+        //playerLives.text = player.lives.ToString();
+        //playerLives2.text = player.lives.ToString();
     }
 }
