943464575331a8f62039a3bd36c5979e8c6893ce
Diet3r <86099270+Diet3r@users.noreply.github.com>
2021-08-11 15:33:39 +0200

    gtfo error
    fehler behoben

--- Assets/Scripts/TrapActivater.cs
+++ Assets/Scripts/TrapActivater.cs
@@ -1,33 +1,3 @@
-<<<<<<< Updated upstream:Assets/Scripts/TrapActivater.cs
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TrapActivater : MonoBehaviour
-{
-    Transform parent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        parent = transform.parent;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider col)
-    {
-        if(col.gameObject.tag == "Player")
-        {
-            Debug.Log("Player in Trap");
-            parent.position = new Vector3(parent.position.x,parent.position.y + (-parent.position.y),parent.position.z);
-        }
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -79,4 +49,3 @@ public class TrapActivater : MonoBehaviour
         Debug.Log("Trap isn't activated");
     }
 }
->>>>>>> Stashed changes:Assets/Scripts/Map/TrapActivater.cs
