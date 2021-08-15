f6c44d3247f144f1febedd7e119a63578aa22c56
Diet3r <86099270+Diet3r@users.noreply.github.com>
2021-08-15 20:36:29 +0200

    Door and Trap fix
    Script fixes, new prefab, doors in level

--- Assets/Scripts/Map/TrapActivater.cs
+++ Assets/Scripts/Map/TrapActivater.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class TrapActivater : MonoBehaviour
 {
     Transform parent;
+    private bool isActivated = false;
+    private Vector3 defaultPosition;
+    private Vector3 activPosition;
+
+    [SerializeField] private float activationDuration;
+
     // Start is called before the first frame update
     void Start()
     {
         parent = transform.parent;
+        defaultPosition = new Vector3(parent.position.x, parent.position.y, parent.position.z);
+        activPosition = new Vector3(parent.position.x, parent.position.y + (-parent.position.y), parent.position.z);
     }
 
     // Update is called once per frame
@@ -21,8 +29,26 @@ public class TrapActivater : MonoBehaviour
     {
         if(col.gameObject.tag == "Player")
         {
+            if(isActivated)
+            {
+                return;
+            }
             Debug.Log("Player in Trap");
-            parent.position = new Vector3(parent.position.x,parent.position.y + (-parent.position.y),parent.position.z);
+            StartCoroutine(GetTemporarilyActivated());
+            //parent.position = new Vector3(parent.position.x,parent.position.y + (-parent.position.y),parent.position.z);
         }
+    }
+
+    private IEnumerator GetTemporarilyActivated()
+    {
+        Debug.Log("Trap is activ");
+        isActivated = true;
+        parent.position = activPosition;
+
+        yield return new WaitForSeconds(activationDuration);
+
+        parent.position = defaultPosition;
+        isActivated = false;
+        Debug.Log("Trap is inactiv");
     }
 }
