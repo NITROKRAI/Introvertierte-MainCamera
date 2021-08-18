b3b1b47e9ba246f3fb15bb3a84c3b95879086285
Diet3r <86099270+Diet3r@users.noreply.github.com>
2021-08-18 18:01:35 +0200

    HealthbarIngame
    Healthbar is working Ingame

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -5,15 +5,28 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-
+    private MobData playerData;
+    private PlayerStats playerStats;
     public Slider slider;
 
-    public void SetHealth(int health)
+    private void Start()
+    {
+        playerData = FindObjectOfType<MobData>();
+        playerStats = FindObjectOfType<PlayerStats>();
+        SetMaxHealth(playerData.Health);
+    }
+
+    private void Update()
+    {
+        SetHealth(playerStats.CurrentHealth);
+    }
+
+    public void SetHealth(float health)
     {
         slider.value = health;
     }
 
-    public void SetMaxHealth(int maxHealth)
+    public void SetMaxHealth(float maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.value = maxHealth;
