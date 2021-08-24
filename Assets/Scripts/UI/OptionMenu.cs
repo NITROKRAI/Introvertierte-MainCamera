41a1aed889ab1cf0755a59af6562c3666fa8acd9
NITROKRAI <maikheitmann8@gmail.com>
2021-08-24 16:19:51 +0200

    fixes

--- Assets/Scripts/UI/OptionMenu.cs
+++ Assets/Scripts/UI/OptionMenu.cs
@@ -1,28 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 using UnityEngine.UI;
 using UnityEngine.Audio;
-=======
->>>>>>> development
 using UnityEngine.SceneManagement;
 
 public class OptionMenu : MonoBehaviour
 {
-<<<<<<< HEAD
+
     public GameObject optionsMenu;
     public Slider[] volumeSliders;
     public AudioMixer masterMixer;
     // Start is called before the first frame update
     void Start()
-=======
+    {
+
+    }
+
     public void BackToMain()
->>>>>>> development
+
     {
         SceneManager.LoadScene("MainMenu");
     }
-<<<<<<< HEAD
+
     public void SetMasterVolume(float sliderValue)
     {
         masterMixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
@@ -38,7 +38,5 @@ public class OptionMenu : MonoBehaviour
         masterMixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("SFXVol", sliderValue);
     }
-=======
-   
->>>>>>> development
 }
+
